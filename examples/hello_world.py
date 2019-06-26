33f13d3b7578e126768f4cf54c4e060e35c84b7a
Erik Moqvist <erik.moqvist@gmail.com>
2019-06-26 22:40:21 +0200

    Hello world example.

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,2 +1,18 @@
 import asyncio
 import httpasync
+
+
+class Index:
+
+    def get(self, request, response):
+        response.text = 'Hello world!'
+
+
+async def main():
+    server = httpasync.Server('localhost', 8080)
+    server.add_route('/', Index())
+
+    await server.serve_forever()
+
+
+asyncio.run(main())
